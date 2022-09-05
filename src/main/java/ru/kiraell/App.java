0fc47ed40736a86ee6c2f3acd5e9dcf9b44164fd
LordKiraell <senseymg@gmail.com>
2022-09-05 19:38:46 +0300

    Start

--- src/main/java/ru/kiraell/App.java
+++ src/main/java/ru/kiraell/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Dolly" );
+        System.out.println( "Hello World! no hope fuck  ");
     }
 }
